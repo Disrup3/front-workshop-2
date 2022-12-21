7513c48ae0c2852e5aa4a71b71f36d608a2c3a92
alex <alexencmer@gmail.com>
2022-12-21 14:58:50 +0100

    logica basica, ya funciona

---
+++ components/ClientList.tsx
@@ -0,0 +1,92 @@
+import { BigNumber, ethers } from "ethers";
+import { FC, useEffect, useState } from "react";
+import { useContractWrite, usePrepareContractWrite } from "wagmi";
+import  wcbet  from "../constants/abi.json"
+import {useContractAddressStore} from "../stores/contractAddressStore"
+import {toast} from "react-toastify";
+
+interface Props {
+    clientList: any[];
+}
+
+const ClientList: FC<Props> = ({clientList}) => {
+    const [selectedClient, setSelectedClient] = useState<number>()
+    const [ donationValue, setDonationValue] = useState(0);
+
+
+    const contractAddress = useContractAddressStore((state) => state.contractAddress)
+
+    const {config} = usePrepareContractWrite({
+        address: contractAddress,
+        functionName: "tipCoffee",
+        abi: wcbet.abi,
+        args: [selectedClient!],
+        overrides: {            
+            value: ethers.utils.parseEther(String(donationValue)),
+        }                
+    })
+
+    const {write, data, error, isLoading, isSuccess} = useContractWrite(config)
+        
+
+    // team id + msg.value
+    const bet = () => {        
+        write?.();
+    }
+
+
+    const selectTeam = (clientId: number) => {
+        if(clientId < 0 ) return;
+        setSelectedClient(clientId);
+    }
+
+  return (        
+
+        <div className="flex-1">        
+            <div className="rounded p-2 h-[60vh] overflow-scroll border-solid border-4 border-white/[0.4] flex flex-wrap justify-center mt-10">
+                {clientList.map((team, i)  => (
+                    <TeamCard key={i} client={team} selectClient={selectTeam} selectedClient={selectedClient} />
+                ))}
+                
+            </div>
+            <div className="flex justify-betweenx mt-4 gap-3">
+                <div className=" flex-[3] flex flex-col ">                         
+                    <input className=" rounded-sm p-4 text-black" value={typeof donationValue !== "undefined" ? donationValue : "0.01... eth"} onChange={(e) => setDonationValue(Number(e.target.value))} type="number" step="0.01" placeholder={`Cantidad a donar ${ selectedClient ? "a " + clientList[Number(selectedClient!.toString())].name : ""}`} />                   
+                </div>
+                <button disabled={isLoading} onClick={bet} className=" rounded-sm bg-purple-700 flex-1 text-center">{isLoading ? "..." : "Donar"}</button>
+            </div>
+        </div>
+  )
+}
+
+type clientInfo = {
+    id: BigNumber;
+    name: string;
+    description: string;
+    ulrImg: string;
+    wallet: string;
+    tipAmount: BigNumber;
+}
+interface TeamProps {
+    client: clientInfo;
+    selectClient: (teamId: number) => void;
+    selectedClient?: number;
+}
+const TeamCard: FC<TeamProps> = ({client, selectClient, selectedClient}) => {      
+
+    return (
+        <div 
+            onClick={() => selectClient(Number(client.id))} 
+            className={`w-[100%]  cursor-pointer  flex items-center justify-between shadow-md rounded text-[18px] bg-white/[.08] m-3 p-5 ${selectedClient?.toString() === client.id.toString() && "bg-purple-600/[0.16]"}`}
+        >   
+            <div>
+                <img className="w-[50px]" src={client.ulrImg} alt="imagen" />
+                <h3 className="text-center">{client.name}</h3>
+            </div>
+            <p>{client.description}</p>
+            <p className={`m-3 ml-5 text-center ${Number(client.tipAmount) > 0 && " text-[#33E459]"}`}>{Number(client.tipAmount) > 0 ? ethers.utils.formatEther(client.tipAmount.toString()) : 0} ETH</p>            
+        </div>
+    )
+}
+
+export default ClientList
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,33 @@
 import { abi } from "../constants/abi.json";
 import { useContractRead } from "wagmi";
 import SelectContractAddress from "../components/SelectContractAddress";
-import TeamList from "../components/TeamList";
 import AppLayout from "../components/layouts/AppLayout";
 import {useContractAddressStore} from "../stores/contractAddressStore"
 import { ethers } from "ethers";
 import WithdrawFunds from "../components/WithdrawFunds";
+import ClientList from "../components/ClientList";
 
 
 export default function Home() {
   
   const contractAddress = useContractAddressStore((state) => state.contractAddress)
 
-  const {data: totalBetAmount = 0} = useContractRead({
+  const {data: AmountTotal = 0, error} = useContractRead({
     address: contractAddress,
     abi: abi,
-    functionName: "totalBettedAmount",
+    functionName: "AmountTotal",
     chainId: 5,
     watch: true
   })
 
-  const {data: winnerId = 99} = useContractRead({
+  const {data: clientList = []} = useContractRead({
     address: contractAddress,
     abi: abi,
-    functionName: "winnerId",
+    functionName: "getCoffeesList",
     chainId: 5,
-    watch: true, 
-    cacheTime: 60000 // 1min 
+    watch: true
   })
 
-  console.log(Number(winnerId))
-
-  const {data: teamList = [], error, isLoading}  = useContractRead({
-    address: contractAddress,
-    abi: abi,
-    functionName: "getTeamList",
-    chainId: 5,
-    watch: true,
-    cacheTime: 10000
-  })
 
 
   if(error) return(
@@ -63,26 +52,25 @@ export default function Home() {
           : (
             <div className="mt-5">
               <div className="flex flex-wrap mb-10 gap-10 mr-10 ml-10 pt-[5rem]">
+
                 <div className="flex-1">
                   <h1 className=" text-[82px]  text-[#D9F40B]">Workshop</h1>
 
-                  <p className="mt-5">Adivina que equipo ganará la copa del mundo de Qatar 2022. ¿ Cuál de los 16 equipos será el ganador ?</p>
+                  <p className="mt-5">Buy me a Coffe en ethereum</p>
 
                   <div className="flex flex-col mt-10">
                   { contractAddress.length > 1 &&  <p className="text-xl">Conectado a:
                    <span className="ml-3 text-[#D9F40B]" >{contractAddress}</span></p> } 
 
                     <p className="text-xl">Cantidad total apostada:
-                     <span className="ml-3 text-purple-500">{ethers.utils.formatEther(String(totalBetAmount))} eth </span>
+                     <span className="ml-3 text-purple-500">{ethers.utils.formatEther(String(AmountTotal))} eth </span>
                     </p>                    
                   </div>
                   
-                  { Number(winnerId) < 16 && (
-                    <WithdrawFunds winnerId={Number(winnerId)}/>
-                  )}
                   
-                </div>
-                <TeamList totalBetValue={totalBetAmount as number} teamList={teamList as any[]}/>
+                </div>                
+                  <ClientList clientList={clientList as any[]}/>               
+                
               </div>
 
             </div>
